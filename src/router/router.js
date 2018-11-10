9465cf9e9549c47608d516fd5a3e10a240a842b3
sirfengyu <sirfengyu@gmail.com>
2018-11-10 22:41:18 +0800

    1lesson

--- src/router/router.js
+++ src/router/router.js
@@ -1,11 +1,7 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Home from '../views/Home.vue'
 
-Vue.use(Router)
 
-export default new Router({
-  routes: [
+export default [
     {
       path: '/',
       name: 'home',
@@ -17,7 +13,6 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
     }
   ]
-})
